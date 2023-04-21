b6a7d1d7a705204f624b89bcd4e287642a572463
Sungwon Lee <sstxz444@gmail.com>
2023-04-21 16:28:31 +0900

    User Entity 추가
    User Entity 추가

---
+++ src/main/java/sungwon/mbti/mbti/User.java
@@ -0,0 +1,36 @@
+package sungwon.mbti.mbti;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    // getters and setters
+
+}
